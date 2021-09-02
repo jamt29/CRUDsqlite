1c1d9064d1e7c4cc05f7a3fd9ed2843ce030d8a4
jamt29 <adony.torres29@gmail.com>
2021-09-02 14:27:24 -0600

    Initial Commit (Upload Proyect)

---
+++ app/src/main/java/com/itca/crud_sqlite/AdminSQLiteOpenHelper.java
@@ -0,0 +1,25 @@
+package com.itca.crud_sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table articulos (codigo int not null primary key, descripcion text, precio real)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop table if exists articulos");
+        onCreate(db);
+    }
+}
