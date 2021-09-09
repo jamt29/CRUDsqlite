8ec69151d7d8d782e68e4a9bbd7db7cd561308c3
jamt29 <adony.torres29@gmail.com>
2021-09-09 16:07:39 -0600

    update more options for crud

--- app/src/main/java/com/itca/crud_sqlite/MainActivity.java
+++ app/src/main/java/com/itca/crud_sqlite/MainActivity.java
@@ -1,6 +1,8 @@
 package com.itca.crud_sqlite;
 
 import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -25,13 +27,15 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this ,"administracion.db",null,1);
-            SQLiteDatabase.bd = admin.getWritableDatabase();
+          //  SQLiteDatabase.bd = admin.getWritableDatabase();
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     private EditText et_codigo, et_descripcion, et_precio;
     private Button btnguardar, btnconsultar1, btnconsultar2, btneliminar, btnmodificar, btnnuevo, btnsalir;
+
+    String codigo, descripcion, precio;
 
 
     @Override
@@ -41,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+       // setSupportActionBar(binding.toolbar);
 
        /* NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
@@ -63,13 +67,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+      /*  binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        }); */
     }
 
     @Override
@@ -105,30 +109,132 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String descripcion = et_descripcion.getText().toString();
                 String precio = et_precio.getText().toString();
                 ContentValues registro = new ContentValues();
-                registro.put("codigo",codigo)
+                registro.put("codigo",codigo);
+                registro.put("descripcion",descripcion);
+                registro.put("precio",precio);
 
-            break;
+
+                if (codigo.isEmpty()){
+                    et_codigo.setError("Campo Obligatorio");
+                }
+                else if (descripcion.isEmpty()){
+                    et_descripcion.setError("Campo Obligatorio");
+                }
+                else if (precio.isEmpty()){
+                    et_precio.setError("Campo Obligatorio");
+                } else {
+                    bd.insert("articulos", null, registro);
+                    bd.close();
+                    et_codigo.setText(null);
+                    et_descripcion.setText(null);
+                    et_precio.setText(null);
+                    Toast.makeText(this, "Registro guardado correctamente", Toast.LENGTH_SHORT).show();
+                } break;
+
+
+
+
+
+
+
             case R.id.btnconsultar1:
-                Toast.makeText(this, "Has hecho click en el boton Consulta por codigo", Toast.LENGTH_SHORT).show();
+
+
+                codigo = et_codigo.getText().toString();
+                if (codigo.isEmpty()){
+                    et_codigo.setError("Campo Obligatorio");
+                } else {
+                    Cursor file = bd.rawQuery("select descripcion, precio from articulos where codigo=" + codigo, null);
+                    if (file.moveToFirst()){
+                        et_descripcion.setText(file.getString(0));
+                        et_precio.setText(file.getString(1));
+                    } else {
+                        Toast.makeText(this, "No existe un articulo con dicho codigo", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
                 break;
             case R.id.btnconsultar2:
-                Toast.makeText(this, "Has hecho click en el boton Consulta por descripcion", Toast.LENGTH_SHORT).show();
+
+
+               descripcion = et_descripcion.getText().toString();
+                if (descripcion.isEmpty()){
+                    et_descripcion.setError("Campo Obligatorio");
+                } else {
+                    Cursor file = bd.rawQuery("select codigo, precio from articulos where descripcion= '" + descripcion + "'" , null);
+                    if (file.moveToFirst()){
+                        et_codigo.setText(file.getString(0));
+                        et_precio.setText(file.getString(1));
+                    } else {
+                        Toast.makeText(this, "No existe un articulo con dicha descripcion", Toast.LENGTH_SHORT).show();
+                    } bd.close();
+                }
+
+
+
                 break;
+
             case R.id.btneliminar:
-                Toast.makeText(this, "Has hecho click en el boton eliminar", Toast.LENGTH_SHORT).show();
+
+             String cod2 = et_codigo.getText().toString();
+             if (cod2.isEmpty()){
+                 et_codigo.setError("Campo Obligatorio");
+             } else {
+                 int cant2 = bd.delete("articulos", "codigo=" + cod2, null);
+                 bd.close();
+                 et_codigo.setText("");
+                 et_descripcion.setText("");
+                 et_precio.setText("");
+                 if (cant2 == 1)
+                     Toast.makeText(this, "Se borro el articulo con dicho codigo", Toast.LENGTH_SHORT).show();
+                 else
+                     Toast.makeText(this, "No existe un articulo con dicho codigo", Toast.LENGTH_SHORT).show();
+
+
+             }
+
+
+
                 break;
             case R.id.btnmodificar:
-                Toast.makeText(this, "Has hecho click en el boton modificar", Toast.LENGTH_SHORT).show();
+
+
+                String cod = et_codigo.getText().toString();
+                String descri = et_descripcion.getText().toString();
+                if (cod.isEmpty()) {
+                    et_codigo.setError("Campo Obligatorio");
+                }else {
+
+                    String pre = et_precio.getText().toString();
+                    ContentValues registro1 = new ContentValues();
+                    registro1.put("codigo",cod);
+                    registro1.put("descripcion",descri);
+                    registro1.put("precio",pre);
+                    int cant = bd.update("articulos", registro1, "codigo=" + cod, null);
+                    if (cant == 1)
+                        Toast.makeText(this, "Se modificaron los datos", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(this, "No existe un articulos con el codigo ingresado", Toast.LENGTH_SHORT).show();
+
+
+                }
+
+
                 break;
             case R.id.btnnuevo:
-                Toast.makeText(this, "Has hecho click en el boton nuevo", Toast.LENGTH_SHORT).show();
+                et_codigo.setText(null);
+                et_descripcion.setText(null);
+                et_precio.setText(null);
                 break;
             case R.id.btnsalir:
-                Toast.makeText(this, "Has hecho click en el boton salir", Toast.LENGTH_SHORT).show();
+
+                finish();
                 break;
         }
 
     }
+
+
 }
 
    /* @Override
